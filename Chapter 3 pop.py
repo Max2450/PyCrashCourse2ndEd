c388381145e24cd68086a0765b374a2d851dd346
Max G. <mrg245000@gmail.com>
2024-01-30 15:18:05 -0800

    Committing new files to project

---
+++ Chapter 3 pop.py
@@ -0,0 +1,6 @@
+# using pop() to remove a value from a list and use it
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
