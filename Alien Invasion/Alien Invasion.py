78f6e60e722a891ce885ba07317859a34d0b5968
Max G. <mrg245000@gmail.com>
2024-01-30 22:01:37 -0800

    added pg. 60 exercises

---
+++ Alien Invasion/Alien Invasion.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    #class to manage game
+
+    def __init__(self):
+        #initialize game and resources
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        #start loop for the game
+        while True:
+            #listen for user input
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #make most recently drawn screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #make game instance and run it
+    ai = AlienInvasion()
+    ai.run_game()
+
