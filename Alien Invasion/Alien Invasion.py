22131c630540fe555bb12f7c278f7a9b7244780c
Max G. <mrg245000@gmail.com>
2024-01-31 21:11:14 -0800

    added bg color, ship image to Alien Invasion. Left off at page 236

--- Alien Invasion/Alien Invasion.py
+++ Alien Invasion/Alien Invasion.py
@@ -1,31 +1,45 @@
 import sys
 import pygame
 
-#project 1
+from settings import Settings
+from ship import Ship
+
+
+# project 1
 
 class AlienInvasion:
-    #class to manage game
+    # class to manage game
 
     def __init__(self):
-        #initialize game and resources
+        # initialize game and resources
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
+        # Set the background color (now in settings)
+        #self.bg_color = (230, 230, 230)
+
     def run_game(self):
-        #start loop for the game
+        # start loop for the game
         while True:
-            #listen for user input
+            # listen for user input
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #make most recently drawn screen visible
+            # redraw the screen each pass
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # make most recently drawn screen visible
             pygame.display.flip()
 
+
 if __name__ == '__main__':
-    #make game instance and run it
+    # make game instance and run it
     ai = AlienInvasion()
     ai.run_game()
-
