d528ee9146d6765b5aa4fa9c727a45c84d5ce2d2
Max G. <mrg245000@gmail.com>
2024-02-09 17:17:50 -0800

    Almost done with Chapter 6

--- Alien Invasion/ship.py
+++ Alien Invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         # initialize ship and starting position
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship and get its rect
@@ -14,6 +15,24 @@ class Ship:
 
         # start new ships at bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flag for continuous movement
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # use movement flag to update ship's position
+        # Update ship x value, not the rect's
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
             # draw ship at current location
