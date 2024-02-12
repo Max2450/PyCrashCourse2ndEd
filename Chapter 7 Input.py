dacd9ff2df883227e94c0f4a6c0c5203b9580ef8
Max2450 <mrg245000@gmail.com>
2024-02-11 16:12:46 -0800

    Did 3 chapter 7 exercises

--- Chapter 7 Input.py
+++ Chapter 7 Input.py
@@ -1,13 +1,28 @@
 choice = -1
 while choice != 0:
-    choice = int(input("\nPlease choose which Chapter 7 Exercise to view. "
+    choice = int(input("Please choose which Chapter 7 Exercise to view. "
                        "Enter 1-12 or 0 to exit: "))
 
     # pg. 117 7-1
     if choice == 1:
-        # TODO
+        car_brand = input("What kind of rental car would you like? ")
+        print(f"Let me see if I can find you a {car_brand}.")
 
     elif choice == 2:
+        num_diners = int(input("How many people will be in your dinner group tonight? "))
+        if num_diners > 8:
+            print("Unfortunately there will be a wait for a table that large.")
+        else:
+            print("Your table is ready. Right this way!")
+
+    elif choice == 3:
+        num = int(input("Give me a number: "))
+        if num % 10 == 0:
+            print(f"{num} is a multiple of 10.")
+        else:
+            print(f"{num} is NOT a multiple of 10.")
+
+    elif choice == 4:
         # TODO
 
 print("Done with Chapter 7! ")
