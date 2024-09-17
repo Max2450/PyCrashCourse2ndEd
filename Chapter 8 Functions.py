142079af534b9526046661946908876146379393
Max G <mrg245000@gmail.com>
2024-09-16 20:57:06 -0700

    On to 8-6!

--- Chapter 8 Functions.py
+++ Chapter 8 Functions.py
@@ -15,10 +15,73 @@ while choice != 0:
         def favorite_book(title):
             """Tell me one of your fave books!"""
             print(f"One of my favorite books is {title}")
+
         book = input("What's one of your favorite books? ")
         favorite_book(book)
 
     elif choice == 3:
-        print("nuffin here yet.")
+        def make_shirt(size, shirt_message):
+            """prints summary of shirt given size and message arguments."""
+            print(f"You ordered a {size} shirt with '{shirt_message}' on the front. ")
+
+        #size = input("What is your shirt size? ")
+        #shirt_message = input("What text would you like printed on the shirt? ")
+
+        #positional arguments
+        make_shirt("L", "Wouldn't you like to know, weatherboy?")
+        #keyword arguments
+        make_shirt(size="XS", shirt_message="This is a shirt for a dog!")
+
+    elif choice == 4:
+        def make_2nd_shirt(size="L", shirt_message="I love Python."):
+            """prints summary of a shirt created with default parameters"""
+            print(f"You ordered a {size} shirt with {shirt_message} on it.")
+
+        make_2nd_shirt()
+        make_2nd_shirt("M", "North America!")
+
+    elif choice == 5:
+        def describe_city(city_name, country_name="Mexico"):
+            """summarizes city"""
+            print(f"{city_name} is in {country_name}")
+        describe_city("Tulum")
+        describe_city("Chihuahua")
+        describe_city("Kuala Lumpur", "Malaysia")
+
+    elif choice == 6:
+        #TODO
+
+    elif choice == 7:
+        # TODO
+
+    elif choice == 8:
+        # TODO
+
+    elif choice == 9:
+        # TODO
+
+    elif choice == 10:
+        # TODO
+
+    elif choice == 11:
+        # TODO
+
+    elif choice == 12:
+        # TODO
+
+    elif choice == 13:
+        # TODO
+
+    elif choice == 14:
+        # TODO
+
+    elif choice == 15:
+        # TODO
+
+    elif choice == 16:
+        # TODO
+
+    elif choice == 17:
+        # TODO
 
 print("Done with Chapter 8! ")
