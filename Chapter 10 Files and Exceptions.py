f978091c5b8b48fdae8a355dc4704f37fafac5f3
Max2450 <mrg245000@gmail.com>
2024-11-05 23:29:45 -0800

    Created Chapter 10 boilerplate

---
+++ Chapter 10 Files and Exceptions.py
@@ -0,0 +1,47 @@
+
+
+choice = -1
+while choice != 0:
+    choice = int(input("Please choose which Chapter 10 Exercise to view. "
+                       "Enter 1-13 or 0 to exit: "))
+
+    if choice == 1:
+        """TODO"""
+
+    elif choice == 2:
+        """TODO"""
+
+    elif choice == 3:
+        """TODO"""
+
+    elif choice == 4:
+        """TODO"""
+
+    elif choice == 5:
+        """TODO"""
+
+    elif choice == 6:
+        """TODO"""
+
+    elif choice == 7:
+        """TODO"""
+
+    elif choice == 8:
+        print("TODO")
+
+    elif choice == 9:
+        print("TODO")
+
+    elif choice == 10:
+        """TODO"""
+
+    elif choice == 11:
+        """TODO"""
+
+    elif choice == 12:
+        """TODO"""
+
+    elif choice == 13:
+        print("TODO")
+
+print("Done with Chapter 10! ")
